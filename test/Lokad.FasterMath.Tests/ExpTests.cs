515ac640292d9771ed1716467409c3a69c9aff69
Joannes Vermorel <joannes.vermorel@lokad.com>
2019-11-03 12:16:41 +0100

    log & exp, check strict scalar vs superscalar equivalence

--- test/Lokad.FasterMath.Tests/ExpTests.cs
+++ test/Lokad.FasterMath.Tests/ExpTests.cs
@@ -19,14 +19,17 @@ namespace Lokad.FastMath.Tests
         [Fact]
         public void Exp_Vector256()
         {
-            for(float i = -85; i < 85; i += 0.1f)
+            for (float i = -85; i < 85; i += 0.1f)
             {
-                var x = Vector256.Create(i, i + 0.01f, i + 0.02f, i + 0.03f, i + 0.04f, i + 0.05f, i + 0.06f, i + 0.07f);
-                var r = FastMath.Exp(x);
+                var r = FastMath.Exp(Vector256.Create((float)i));
+                var expected = FastMath.Exp((float)i);
 
                 for (var k = 0; k < 8; k++)
                 {
-                    Assert.True(MathF.Exp(i + k * 0.01f).RelError(r.GetElement(k)) <= 1.72886892e-3f);
+                    // HACK: [vermore] can't reproduce perfect numerical identity 
+                    // between scalar and SIMD variants (very close though)
+                    Assert.True(expected.AbsError(r.GetElement(k)) <= 1e-6f);
+                    //Assert.Equal(expected, r.GetElement(k));
                 }
             }
         }
--- test/Lokad.FasterMath.Tests/LogTests.cs
+++ test/Lokad.FasterMath.Tests/LogTests.cs
@@ -21,12 +21,12 @@ namespace Lokad.FastMath.Tests
         {
             for (float i = -1; i < 85; i += 0.1f)
             {
-                var x = Vector256.Create(i, i + 0.01f, i + 0.02f, i + 0.03f, i + 0.04f, i + 0.05f, i + 0.06f, i + 0.07f);
-                var r = FastMath.Log(x);
+                var r = FastMath.Log(Vector256.Create((float)i));
+                var expected = FastMath.Log((float)i);
 
                 for (var k = 0; k < 8; k++)
                 {
-                    Assert.True(MathF.Log(i + k * 0.01f).AbsError(r.GetElement(k)) <= 1e-4f);
+                    Assert.Equal(expected, r.GetElement(k));
                 }
             }
         }
